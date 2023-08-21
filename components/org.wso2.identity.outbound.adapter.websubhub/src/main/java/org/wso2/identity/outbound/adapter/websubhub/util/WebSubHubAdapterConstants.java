d9dbbdeb25f28bb078eb91a98cbb44e017b93d0b
Vivekvinushanth Christopher <32198547+VivekVinushanth@users.noreply.github.com>
2023-08-21 10:11:34 +0530

    Update components/org.wso2.identity.outbound.adapter.websubhub/src/main/java/org/wso2/identity/outbound/adapter/websubhub/util/WebSubHubAdapterConstants.java
    Co-authored-by: Thamindu Dilshan Jayawickrama <35653110+ThaminduDilshan@users.noreply.github.com>

--- components/org.wso2.identity.outbound.adapter.websubhub/src/main/java/org/wso2/identity/outbound/adapter/websubhub/util/WebSubHubAdapterConstants.java
+++ components/org.wso2.identity.outbound.adapter.websubhub/src/main/java/org/wso2/identity/outbound/adapter/websubhub/util/WebSubHubAdapterConstants.java
@@ -109,7 +109,7 @@ public class WebSubHubAdapterConstants {
         TOPIC_DEREGISTRATION_FAILURE_ACTIVE_SUBS("65009", "Error occurred while de-registering topic", "Backend error" +
                 " received from WebSubHub while attempting to de-register topic: %s. Active subscribers: %s."),
         ERROR_RETRIEVING_ENCRYPTION_PUBLIC_KEY("65013", "Unable to retrieve event encryption public key",
-                "Unable to retrieve event encryption public key from %s");
+                "Unable to retrieve event encryption public key from %s.");
 
         private final String code;
         private final String message;
